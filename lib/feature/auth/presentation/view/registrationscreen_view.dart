5a930426094338118c95e43ca43638664c370fce
safalbhattarai <126142996+safalbhattarai@users.noreply.github.com>
2025-03-01 09:07:57 +0545

    changing in registration screen view.dart file

--- lib/feature/auth/presentation/view/registrationscreen_view.dart
+++ lib/feature/auth/presentation/view/registrationscreen_view.dart
@@ -1,16 +1,364 @@
-import 'dart:io';
+// import 'dart:io';
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:permission_handler/permission_handler.dart';llllll
+
+
+// class RegistrationscreenView extends StatefulWidget {
+//   const RegistrationscreenView({super.key});
+
+//   @override
+//   State<RegistrationscreenView> createState() => _RegisterViewState();llll
+// }
+
+// class _RegisterViewState extends State<RegistrationscreenView> {
+//   final _gap = const SizedBox(height: 15);
+//   final _key = GlobalKey<FormState>();
+
+//   final _fnameController = TextEditingController();
+//   final _lnameController = TextEditingController();
+//   final _phoneController = TextEditingController();
+//   final _emailController = TextEditingController();
+//   final _addressController = TextEditingController();
+//   final _usernameController = TextEditingController();
+//   final _passwordController = TextEditingController();
+
+//   checkCameraPermission() async {
+//     if (await Permission.camera.request().isRestricted ||
+//         await Permission.camera.request().isDenied) {
+//       await Permission.camera.request();
+//     }
+//   }
+
+//   File? _img;
+//   Future _browseImage(ImageSource imagesource) async {
+//     try {
+//       final image = await ImagePicker().pickImage(source: imagesource);
+//       if (image != null) {
+//         setState(() {
+//           _img = File(image.path);
+//           // sending image to server
+//           context.read<RegistrationBloc>().add(ll
+//                 LoadImage(file: _img!),
+//               );
+//         });
+//       } else { 
+//         return;
+//       }
+//     } catch (e) {
+//       debugPrint(e.toString());
+//     }
+//   }lll
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // Get the screen width to adjust the layout for tablets
+//     double screenWidth = MediaQuery.of(context).size.width;
+//     double padding =
+//         screenWidth > 600 ? 40 : 20; // Add extra padding for tablets
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text(
+//           'Registration',
+//           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//         ),
+//         centerTitle: true,
+//         backgroundColor: Colors.orange,
+//       ),
+//       body: SafeArea(
+//         child: SingleChildScrollView(
+//           child: Padding(
+//             padding: EdgeInsets.all(padding),
+//             child: Form(
+//               key: _key,
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   // Profile Image Section
+//                   GestureDetector(
+//                     onTap: () {
+//                       showModalBottomSheet(
+//                         backgroundColor: Colors.orange.shade100,
+//                         context: context,
+//                         isScrollControlled: true,
+//                         shape: const RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.vertical(
+//                             top: Radius.circular(20),
+//                           ),
+//                         ),
+//                         builder: (context) => Padding(
+//                           padding: const EdgeInsets.all(20),
+//                           child: Row(
+//                             mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                             children: [
+//                               ElevatedButton.icon(
+//                                 onPressed: () {
+//                                   checkCameraPermission();
+//                                   _browseImage(ImageSource.camera);
+//                                   Navigator.pop(context);
+//                                 },
+//                                 icon: const Icon(Icons.camera),
+//                                 label: const Text('Camera'),
+//                               ),
+//                               ElevatedButton.icon(
+//                                 onPressed: () {
+//                                   checkCameraPermission();
+//                                   _browseImage(ImageSource.gallery);
+//                                 },
+//                                 icon: const Icon(Icons.image),
+//                                 label: const Text('Gallery'),
+//                               ),
+//                             ],
+//                           ),
+//                         ),
+//                       );
+//                     },
+//                     child: Center(
+//                       child: CircleAvatar(
+//                         radius: screenWidth > 600 ? 80 : 50,
+//                         backgroundImage: _img != null
+//                             ? FileImage(_img!)
+//                             : const AssetImage('assets/images/1.jpg')
+//                                 as ImageProvider,
+//                       ),
+//                     ),
+//                   ),
+//                   const SizedBox(height: 30),
+
+//                   // TextFields for Registration
+//                   _buildTextField('First Name', _fnameController),
+//                   _gap,
+//                   _buildTextField('Last Name', _lnameController),
+//                   _gap,
+//                   _buildTextField('Phone No', _phoneController),
+//                   _gap,
+//                   _buildTextField('Email', _emailController),
+//                   _gap,
+//                   _buildTextField('Address', _addressController),
+//                   _gap,
+//                   _buildTextField('Username', _usernameController),
+//                   _gap,
+//                   _buildTextField('Password', _passwordController,
+//                       obscureText: true),
+//                   _gap,
+
+//                   // Register Button
+//                   SizedBox(
+//                     width: double.infinity,
+//                     child: ElevatedButton(
+//                       onPressed: () {
+//                         if (_key.currentState!.validate()) {
+//                           final registrationState =
+//                               context.read<RegistrationBloc>().state;
+//                           final imageName = registrationState.imageName;
+//                           context.read<RegistrationBloc>().add(
+//                                 RegisterCustomer(
+//                                   context: context,
+//                                   fName: _fnameController.text,
+//                                   lName: _lnameController.text,
+//                                   phoneNo: _phoneController.text,
+//                                   email: _emailController.text,
+//                                   address: _addressController.text,
+//                                   username: _usernameController.text,
+//                                   password: _passwordController.text,
+//                                   image: imageName,
+//                                 ),
+//                               );
+//                         }
+//                       },
+//                       style: ElevatedButton.styleFrom(
+//                         backgroundColor: Colors.orange,
+//                         foregroundColor: Colors.white,
+//                         minimumSize: const Size(double.infinity, 50),
+//                         shape: RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.circular(8),
+//                         ),
+//                       ),
+//                       child: const Text(
+//                         'Register',
+//                         style: TextStyle(
+//                           fontFamily: 'Montserrat Bold',
+//                           fontWeight: FontWeight.bold,
+//                           fontSize: 18,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget _buildTextField(String label, TextEditingController controller,
+//       {bool obscureText = false}) {
+//     return TextFormField(
+//       controller: controller,
+//       obscureText: obscureText,
+//       decoration: InputDecoration(
+//         labelText: label,
+//         labelStyle: const TextStyle(fontSize: 18),
+//         filled: true,
+//         fillColor: Colors.orange.shade50,
+//         border: OutlineInputBorder(
+//           borderRadius: BorderRadius.circular(12),
+//           borderSide: BorderSide(color: Colors.orange.shade200),
+//         ),
+//         focusedBorder: OutlineInputBorder(
+//           borderRadius: BorderRadius.circular(12),
+//           borderSide: const BorderSide(color: Colors.orange),
+//         ),
+//       ),
+//       validator: (value) {
+//         if (value == null || value.isEmpty) {
+//           return 'Please enter $label';
+//         }
+//         return null;
+//       },
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:stockvision_app/feature/auth/presentation/view_model/registration/bloc/registration_bloc.dart';
+import 'package:permission_handler/permission_handler.dart';llllll
+
 
 class RegistrationscreenView extends StatefulWidget {
   const RegistrationscreenView({super.key});
 
   @override
-  State<RegistrationscreenView> createState() => _RegisterViewState();
+  State<RegistrationscreenView> createState() => _RegisterViewState();llll
 }
 
 class _RegisterViewState extends State<RegistrationscreenView> {
@@ -40,17 +388,17 @@ class _RegisterViewState extends State<RegistrationscreenView> {
         setState(() {
           _img = File(image.path);
           // sending image to server
-          context.read<RegistrationBloc>().add(
+          context.read<RegistrationBloc>().add(ll
                 LoadImage(file: _img!),
               );
         });
-      } else {
+      } else { 
         return;
       }
     } catch (e) {
       debugPrint(e.toString());
     }
-  }
+  }lll
 
   @override
   Widget build(BuildContext context) {
@@ -218,9 +566,83 @@ class _RegisterViewState extends State<RegistrationscreenView> {
       validator: (value) {
         if (value == null || value.isEmpty) {
           return 'Please enter $label';
-        }
+        };
         return null;
       },
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
