6987538d219142a6daba9f4082aca01784d2a6c5
safalbhattarai <126142996+safalbhattarai@users.noreply.github.com>
2025-03-07 01:23:14 +0545

    changing in regsiter screen

--- lib/feature/auth/presentation/view/registrationscreen_view.dart
+++ lib/feature/auth/presentation/view/registrationscreen_view.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:permission_handler/permission_handler.dart';llllll
 
 
 class RegistrationscreenView extends StatefulWidget {
